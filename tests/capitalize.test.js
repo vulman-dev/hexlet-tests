2cfc7bf6a5789391e5823b87af1e9f79a54a0277
Евгений Вульман <vulman@DESKTOP-0SKP5G9.localdomain>
2022-08-06 11:49:10 +0300

    last commit

---
+++ tests/capitalize.test.js
@@ -0,0 +1,12 @@
+import { default as capitalize } from '../src/capitalize.js';
+
+if (capitalize('hello') !== 'Hello') {
+    throw new Error('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
+
